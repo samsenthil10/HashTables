15949cc944ec9b540d41f212008773cd9e49f6d6
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-02 04:06:34 +0530

    Added: To find frequency of occurance of word in given string

--- src/com/bridgelabz/hashtables/HashMapMain.java
+++ src/com/bridgelabz/hashtables/HashMapMain.java
@@ -1,8 +1,22 @@
 package com.bridgelabz.hashtables;
 
 public class HashMapMain {
-	
+
 	public static void main(String[] args) {
-		
+		String sentence = "To be or not to be";
+		MyHashMap<String, Integer> hashMap = new MyHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for(String word : words) {
+			Integer value = hashMap.get(word);
+			if(value == null) {
+				value = 1;
+			}
+			else {
+				value += 1;
+			}
+			hashMap.add(word, value);
+		}
+		int frequency = hashMap.get("to");
+		System.out.println(frequency);
 	}
 }
